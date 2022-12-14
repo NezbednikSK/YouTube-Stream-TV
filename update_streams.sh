0a38b4e6c21da2a60bd2384d267a1af654d038d3
NezbednikSK <47603275+NezbednikSK@users.noreply.github.com>
2022-12-14 20:57:01 +0000

    Update update_streams.sh

--- update_streams.sh
+++ update_streams.sh
@@ -6,6 +6,8 @@ while read p; do
   
   node ./grab.js "$(echo "$RAW" | head -n 1)" > "$FILE"
   
+  cat "$FILE"
+  
   export "SHA=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$M3U.m3u8?ref=stream" | node -e "var body=\"\";process.stdin.on(\"data\",(c)=>{body+=c.toString();});process.stdin.on(\"end\",()=>{process.stdout.write(JSON.parse(body).sha);});")"
   curl -s -X PUT -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$M3U.m3u8" -d "{\"message\": \"Update streams ($(TZ="Europe/Bratislava" date "+%d/%m/%y %H:%M:%S %Z"))\", \"branch\": \"stream\", \"sha\": \"$SHA\", \"content\": \"$(base64 -w 0 "$FILE")\"}"
   rm "$FILE"

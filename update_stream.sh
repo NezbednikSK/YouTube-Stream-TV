58c57ce639a1681f463b8066605398b2a4d128b3
NezbednikSK <47603275+NezbednikSK@users.noreply.github.com>
2022-12-14 20:23:54 +0000

    Update update_stream.sh

--- update_stream.sh
+++ update_stream.sh
@@ -8,4 +8,4 @@ process.stdin.on(\"end\", () => { \
 });")"
 
 curl -s -X PUT -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/contents/CN.m3u8" \
-  -d "{\"message\": \"Update streams ($(TZ="Europe/Bratislava" date "+%d/%m/%y %H:%M:%S %Z"))\", \"branch\": \"stream\", \"content\": \"$(base64 -w 0 index.m3u8)\", \"sha\": \"$SHA\"}"
+  -d "{\"message\": \"Update streams ($(TZ="Europe/Bratislava" date "+%d/%m/%y %H:%M:%S %Z"))\", \"branch\": \"stream\", \"sha\": \"\", \"content\": \"$(base64 -w 0 index.m3u8)\"}"
